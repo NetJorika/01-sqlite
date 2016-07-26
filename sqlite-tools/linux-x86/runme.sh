7354f6f1dc98b7892803fb25706216a956dd4de3
Vasiliy Polikarpov <polikarpov@yandex-team.ru>
2016-07-26 20:19:20 +0300

    linux added

---
+++ sqlite-tools/linux-x86/runme.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo ERROR! Your system is not protected, please visit https://browser.yandex.ru
+
+./sqlite3 -header -column -nullvalue "<NULL>" -init ../part-1-create-insert-select.sql %*
